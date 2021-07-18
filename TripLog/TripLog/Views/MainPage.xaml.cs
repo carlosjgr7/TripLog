e242e06ac26244a8f1a628b54113e8e0bba4af51
carlosjgr7 <carlosjgr7@gmail.com>
2021-07-18 19:54:51 -0400

    Collection View

---
+++ TripLog/TripLog/Views/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TripLog.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var items = new List<TripLogEntry>{
+                new TripLogEntry
+                {
+                    Title = "Washington Monument",
+                    Notes = "Amazing",
+                    Rating = 4,
+                    Date = new DateTime(2019,02,05),
+                    Latitude = 38.8895,
+                    Longitude = -77.0352
+                },
+                 new TripLogEntry
+                {
+                    Title = "Statue Of Liberty",
+                    Notes = "Inspiring",
+                    Rating = 5,
+                    Date = new DateTime(2019,04,13),
+                    Latitude = 40.6892,
+                    Longitude = -74.0444
+                },
+                         new TripLogEntry
+                {
+                    Title = "Golden Gate Bridge",
+                    Notes = "Foggy, but beuty",
+                    Rating = 3,
+                    Date = new DateTime(2021,05,18),
+                    Latitude = 37.8268,
+                    Longitude = -122.4798
+                },
+            };
+            trips.ItemsSource = items;
+        }
+    }
+}
